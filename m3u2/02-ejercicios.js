849fc81553958ab35bf4d29637d6418c8773ed21
Camila Cazzaniga <ccazzanigaux@gmail.com>
2026-06-07 16:25:13 -0300

    Ejercicio de la segunda parte JS

---
+++ m3u2/02-ejercicios.js
@@ -0,0 +1,26 @@
+// const parrafos = document.getElementsByTagName (`p`) 
+
+// console.log (parrafos)
+
+// parrafos.classList.length(){
+//     for (let i = 0; i < parrafos.length; i++) {
+//         parrafos[i].classList.add (`parrafo${i}`)
+//     }
+//     console.log (`[i]`)
+// }
+
+//PROFE NO TERMINE EL EJERCICIO 2 DE CONTAR LOS ESPACIOS DE "Programar un contador de caracteres para un textarea." ESTOY AVANZANDO DE A POCO, NO SE ME ILUMINO EL CEREBRO TODAVIA PARA PENSAR LA SOLUCION PERO LO VOY A IR  HACIENDO 
+
+
+function calcularpromedio (parcial1, parcial2) {
+     const promedio = (parcial1 + parcial2) / 2
+     return promedio;
+}
+
+const parcial1Historia = 4;
+const parcial2Historia = 6;
+const promedioHistoria = calcularpromedio(parcial1Historia, parcial2Historia);
+
+
+// console.log (promedioHistoria)
+console.log (`La nota promedio de Historia es ${promedioHistoria}`)
